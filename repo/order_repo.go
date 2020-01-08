693186529c8f1a34cfdbacbee622bdda9e5580e8
Dennis <dionisios.simoni@gmail.com>
2020-01-08 23:47:14 +0000

    implementation of an order repo and tests

---
+++ repo/order_repo.go
@@ -0,0 +1,34 @@
+package repo
+
+import (
+	"order-manager/database"
+	"order-manager/models/order"
+)
+// OrderRepo takes the DB struct as a dependency, which represents the database connection.
+type OrderRepo struct {
+	DB *database.DB
+}
+
+// Upsert inserts or updates a model into the database
+func (or *OrderRepo) Upsert(order *order.Order) error {
+	return or.DB.UpsertOrder(*order)
+}
+
+// Fetch returns an order with a specific id from the database
+func (or *OrderRepo) Fetch(orderID string) (*order.Order, error) {
+	order,err := or.DB.FetchOrder(orderID)
+	if err != nil {
+		return nil, err
+	}
+	return &order, nil
+}
+
+// FetchAll returns all the existing orders from the database
+func (or *OrderRepo) FetchAll() ([]order.Order, error) {
+	return or.DB.FetchAllOrders()
+}
+
+// Delete deletes a specific order from the database
+func (or *OrderRepo) Delete(id string) error {
+	return or.DB.DeleteOrder(id)
+}
